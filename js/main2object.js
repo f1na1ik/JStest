2b47a07fb3dd753cce2393dd00f01e9839edef51
f1na1ik <maxsim119@yandex.ru>
2019-08-23 23:45:10 +0300

    object easy task

--- js/main2object.js
+++ js/main2object.js
@@ -1,6 +1,6 @@
 //создаем объект tool
 let tool = {
-    "name":"pancil object",
+    "name":"pancil",
     "color":"red",
     "diametr":5,
     "sharpened":true
@@ -21,3 +21,24 @@ let tools = [
 ];
 
 console.log([tools[1]["name"],tools[2]["name"]]);
+
+// создадим объект другим методом
+let dog = {};
+dog.name = "Шарик";
+dog.color = "Белый";
+console.log(dog)
+
+// еще один метод
+let cat = {};
+cat["name"] = "Киса";
+cat["color"] = "Черный";
+console.log(cat);
+
+//Задание - решение
+let myCrazyObject = {
+    "name" : "Нелепый объект",
+    "some array" : [7,9, {purpose: "путаница", number: 123}, 3.3],
+    "random animal" : "Банановая акула"
+};
+
+console.log(myCrazyObject["some array"][2].number);
